8a60ae9a2429aa495d9b74bba1ce57827ce78f10
christian <calbace@esliceu.net>
2026-05-20 15:18:05 +0200

    Create cursbash1.2.sh

---
+++ cursbash1.2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#variables són "paraules" que contenen un valor
+NOM="Choped"
+
+#read serveix per a llegir el q diu el keyboard
+
+read -p "introdueix un nom: " NOM
+
+echo "El teu nom és ${NOM}"
